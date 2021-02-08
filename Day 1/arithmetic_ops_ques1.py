4dd839546be8c2b3501bed8ce550ffaffdcb0966
KotiDhana <dhanakotiroxx@gmail.com>
2021-02-08 11:15:01 +0530

    day1_pythonBasics

---
+++ Day 1/arithmetic_ops_ques1.py
@@ -0,0 +1,14 @@
+# Question 1
+# Write a python program that takes two numbers as the input and print the result of Addition,
+# Subtraction, Multiplication, Division of the two numbers
+
+a = int(input())
+b = int(input())
+print("Addition")
+print("Sum:",a+b)
+print("Subtraction")
+print("Difference:",a-b)
+print("Multiplication")
+print("Product:",a*b)
+print("Division")
+print("Quotient:",a/b)
---
+++ Day 1/power_ques2.py
@@ -0,0 +1,8 @@
+# Question 2
+# Write a python program that takes two numbers as the input such as X and Y and print the result
+# of X^Y(X to the power of Y).
+
+X = int(input())
+Y = int(input())
+print("X to the power of Y")
+print("Result:",X**Y)
